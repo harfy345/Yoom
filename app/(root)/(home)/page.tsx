7a4894b07483ae76618807eac3a0335241258216
harfy345 <hakam.almotlk345@gmail.com>
2024-04-14 14:47:36 -0400

    ff

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+"use client";
+import MeetingTypeList from "@/components/MeetingTypeList";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-   <section className='flex size-full flex-col gap-10 text-white'>
-      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
-        <div className='flex h-full'></div>
-      </div>
-   </section>
-  )
-}
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-export default Home
+  // Effect to update the time every second
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Cleanup function to clear the interval when component unmounts
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array to run the effect only once
+
+  // Format the time and date
+  const time = currentTime.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    currentTime
+  );
+
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/HomeCard.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+const HomeCard = ({img , className,title,description,handleClick}) => {
+  return (
+    <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px]
+    min-h-[260px] rounded-[14px] cursor-pointer'
+    onClick={handleClick}
+    >
+       <div className='flex-center glassmorphism size-12 rounded-[10px]'>
+           <Image src={img} alt='meeting' width={27} height={27} />
+       </div>
+       <div className='flex flex-col gap-2'>
+           <h1 className='text-2xl font-bold'>{title}</h1>
+           <p className='text-lg font-normal'>{description}</p>
+       </div>
+   </div>
+  )
+}
+
+export default HomeCard
